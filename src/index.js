ec03a99a459f7d3ac55ecf022a451515624c379e
Timothy Stewart <timothystewart6@hotmail.com>
2017-06-29 19:44:44 -0500

    Added AppBar example

--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { MuiThemeProvider } from 'material-ui/styles';
 import './index.css';
-import App from './App';
+import AppBar from './AppBar';
+
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+    <MuiThemeProvider>
+    <AppBar />
+    </MuiThemeProvider>, document.getElementById('root'));
 registerServiceWorker();
